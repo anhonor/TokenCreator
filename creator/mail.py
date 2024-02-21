734c2474e53b406c30c1624af7061cdae866a218
anhonor <154564843+anhonor@users.noreply.github.com>
2024-02-20 18:01:12 -0600

    Update mail.py
    Captcha was implemented on the site mailmasker.com, I had to migrate to a different service, mailmask.me.

--- creator/mail.py
+++ creator/mail.py
@@ -1,6 +1,5 @@
 import requests
 import ua_generator
-import uuid
 
 class Mail:
       def __init__(mail, proxy: dict | None = None) -> None:
@@ -21,57 +20,13 @@ class Mail:
              'User-Agent': mail.agent.text
           }
 
-      def createUser(mail, username: str, password: str, email_username: str, email: str) -> requests.models.Response:
-          return requests.post('https://api.mailmasker.com/graphql?name=CreateUser', headers = mail.__construct_headers__({
+      def createUser(mail, forwarding_address: str) -> requests.Response:
+          return requests.post('https://423ghva5nf.execute-api.eu-west-1.amazonaws.com/prod/mails', headers = mail.__construct_headers__({
              'Accept': '*/*',
-             'Connection': 'keep-alive',
-             'Content-Type': 'application/json',
-             'DNT': '1',
-             'Host': 'api.mailmasker.com',
-             'Origin': 'https://api.mailmasker.com', 
-             'Referer': 'https://api.mailmasker.com/',
+             'Content-Type': 'application/JSON',
+             'Origin': 'https://www.mailmask.me',
+             'Referer': 'https://www.mailmask.me/',
              'sec-fetch-dest': 'empty',
              'sec-fetch-mode': 'cors',
-             'sec-fetch-site': 'same-site'
-          }), json = {
-             'operationName': 'CreateUser',
-             'query': 'mutation CreateUser($username: String!, $password: String!, $uuid: String!, $persistent: Boolean!, $verifiedEmail: String!, $emailMask: String!) {\n  createUser(username: $username, password: $password, uuid: $uuid, persistent: $persistent, verifiedEmail: $verifiedEmail, emailMask: $emailMask) {\n    userID\n    __typename\n  }\n}\n',
-             'variables': {
-                'emailMask': '{}@mailmasker.com'.format(email_username), 
-                'password': password,
-                'persistent': True,
-                'username': username,
-                'uuid': str(uuid.uuid4()),
-                'verifiedEmail': email
-             }
-          }, proxies = mail.proxy)
-
-      def getMe(mail, cookies: requests.cookies.RequestsCookieJar) -> requests.models.Response:
-          return requests.post('https://api.mailmasker.com/graphql?name=Me', headers = mail.__construct_headers__({
-             'Accept': '*/*',
-             'Connection': 'keep-alive',
-             'Content-Type': 'application/json',
-             'Cookie': mail.__construct_cookies__(cookies),
-             'DNT': '1',
-             'Host': 'api.mailmasker.com',
-             'Origin': 'https://api.mailmasker.com', 
-             'Referer': 'https://api.mailmasker.com/',
-             'sec-fetch-dest': 'empty',
-             'sec-fetch-mode': 'cors',
-             'sec-fetch-site': 'same-site'
-          }), json = {'operationName': 'Me', 'query': 'query Me {\n  me {\n    user {\n      id\n      username\n      plan {\n        displayName\n        type\n        __typename\n      }\n      routes {\n        id\n        redirectToVerifiedEmail {\n          id\n          email\n          verified\n          __typename\n        }\n        emailMask {\n          id\n          domain\n          alias\n          parentEmailMaskID\n          __typename\n        }\n        expiresISO\n        __typename\n      }\n      verifiedEmails {\n        id\n        email\n        verified\n        __typename\n      }\n      emailMasks {\n        id\n        domain\n        alias\n        parentEmailMaskID\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n', 'variables': {}}, proxies = mail.proxy)
-
-      def verifyEmail(mail, code: str, email: str, cookies: requests.cookies.RequestsCookieJar) -> requests.models.Response:
-          return requests.post('https://api.mailmasker.com/graphql?name=VerifyEmailWithCodeMutation', headers = mail.__construct_headers__({
-             'Accept': '*/*',
-             'Connection': 'keep-alive',
-             'Content-Type': 'application/json',
-             'Cookie': mail.__construct_cookies__(cookies),
-             'DNT': '1',
-             'Host': 'api.mailmasker.com',
-             'Origin': 'https://api.mailmasker.com', 
-             'Referer': 'https://api.mailmasker.com/',
-             'sec-fetch-dest': 'empty',
-             'sec-fetch-mode': 'cors',
-             'sec-fetch-site': 'same-site'
-          }), json = {'operationName': 'VerifyEmailWithCodeMutation', 'query': 'mutation VerifyEmailWithCodeMutation($email: String!, $code: String!) {\n  verifyEmailWithCode(email: $email, code: $code) {\n    id\n    email\n    verified\n    __typename\n  }\n}\n', 'variables': {'code': code, 'email': email}}, proxies = mail.proxy)
+             'sec-fetch-site': 'cross-site'
+          }), json = {'forwardingAddress': forwarding_address, 'label': ''})
